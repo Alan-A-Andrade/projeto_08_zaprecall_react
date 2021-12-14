244a81494eb64835d5c041607019cec305b06277
leo <celsolnv27@gmail.com>
2021-12-14 16:16:03 -0300

    Preparando revisão de código

--- src/components/FlashcardHeader.js
+++
@@ -1,9 +0,0 @@
-export default function FlashcardHeader(Props){
-  return(
-    <div className="flashcard-header">
-      <h1>{Props.questionText}</h1>
-      <p data-identifier="counter">{Props.cardNumber}/{Props.cardTotal}</p>
-    </div>
-
-  )
-}
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from "react-dom";
-import App from "./App.js"
-import "./assets/css/reset.css"
-import "./assets/css/style.css"
-
-ReactDOM.render(<App/>, document.querySelector(".root"))
